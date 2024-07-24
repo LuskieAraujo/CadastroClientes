7d2e8fc7c71836cc04980cd35855efb2b1462db7
Lucas Araújo <araujo_depaula@outlook.com>
2024-07-24 14:56:55 -0300

    camada repository

---
+++ CadastroClientes/Repositories/ClienteRepository.cs
@@ -0,0 +1,67 @@
+﻿using CadastroClientes.Data;
+using CadastroClientes.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CadastroClientes.Repositories
+{
+	public class ClienteRepository : IClienteRepository
+	{
+		private readonly BancoContext _context;
+		public ClienteRepository(BancoContext con)
+		{
+			_context = con;
+		}
+
+		public bool Adicionar(ClienteModel cliente)
+		{
+			try
+			{
+				_context.Clientes.Add(cliente);
+				_context.SaveChanges();
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public bool Alterar(ClienteModel cliente)
+		{
+			try
+			{
+				_context.Clientes.Update(cliente);
+				_context.SaveChanges();
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public ClienteModel Obter(int id)
+		{
+			try
+			{
+				return _context.Clientes.FirstOrDefault(x => x.Id == id);
+			}
+			catch
+			{
+				return new ClienteModel();
+			}
+		}
+		public List<ClienteModel> ListarTodos()
+		{
+			try
+			{
+				return _context.Clientes.Where(x =>x.Ativo == true).ToList();
+			}
+			catch
+			{
+				return new List<ClienteModel>();
+			}
+		}
+	}
+}
---
+++ CadastroClientes/Repositories/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using CadastroClientes.Models;
+using System.Collections.Generic;
+
+namespace CadastroClientes.Repositories
+{
+	public interface IClienteRepository
+	{
+		bool Adicionar(ClienteModel cliente);
+		bool Alterar(ClienteModel cliente);
+		ClienteModel Obter(int id);
+		List<ClienteModel> ListarTodos();
+	}
+}
