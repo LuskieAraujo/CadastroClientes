1b3cc05d5c70ec1301515125f6dbb63734cf9992
Lucas Araújo <araujo_depaula@outlook.com>
2024-07-24 19:26:26 -0300

    model do cliente

---
+++ CadastroClientes/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CadastroClientes.Models
+{
+	public class ClienteModel
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Documento { get; set; }
+		public string Telefone { get; set; }
+		public int Tipo { get; set; }
+		public DateTime DataCadastro { get; set; }
+		public bool Ativo {  get; set; }
+	}
+}
